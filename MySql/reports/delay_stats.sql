3de5fb0d3017589ca580c9e8b76203504ed076d0
P Mcgoohan <6204752+pmcgoohan@users.noreply.github.com>
2021-07-26 14:20:52 +0100

    Update delay_stats.sql

--- MySql/reports/delay_stats.sql
+++ MySql/reports/delay_stats.sql
@@ -1,6 +1,21 @@
--- statistical analysis of inclusion delay times (avg and stdev)
+-- statistical analysis of inclusion delay times by block count and time (avg and stdev)
 select  min(eth_tx.arrival_time) as from_time,
-        min(eth_tx.arrival_time) as to_time,
-        count(*) as tx_count,avg(eth_tx.delay_ms) as avg_inclusion_delay_ms,
-        stddev(eth_tx.delay_ms) as stdev_inclusion_delay_ms 
-from tx_time.eth_tx where is_warm = 1
+        max(eth_tx.arrival_time) as to_time,
+        count(*) as tx_count,
+        avg(eth_tx.block_number - eth_tx.arrival_block) as avg_inclusion_delay_blocks,
+        stddev(eth_tx.block_number - eth_tx.arrival_block) as stdev_inclusion_delay_blocks,
+        avg(eth_tx.delay_ms) as avg_inclusion_delay_ms,
+        stddev(eth_tx.delay_ms) as stdev_inclusion_delay_ms  
+from tx_time.eth_tx 
+where is_warm = 1 and eth_tx.arrival_block > 0 and eth_tx.block_number > 0;
+
+-- statistical analysis of inclusion delay times for mempool only txs by block count and time (avg and stdev)
+select  min(eth_tx.arrival_time) as from_time,
+        max(eth_tx.arrival_time) as to_time,
+        count(*) as tx_count,
+        avg(eth_tx.block_number - eth_tx.arrival_block) as avg_inclusion_delay_blocks,
+        stddev(eth_tx.block_number - eth_tx.arrival_block) as stdev_inclusion_delay_blocks,
+        avg(eth_tx.delay_ms) as avg_inclusion_delay_ms,
+        stddev(eth_tx.delay_ms) as stdev_inclusion_delay_ms  
+from tx_time.eth_tx 
+where is_warm = 1 and is_dark = 0 and delay_ms > 0 and eth_tx.arrival_block > 0 and eth_tx.block_number > 0;
